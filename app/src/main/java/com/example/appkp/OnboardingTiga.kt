9aa830355a01943e476360089c120569b922d4ef
langga799 <dikaaldika799@gmail.com>
2020-06-19 16:18:07 +0700

    Menambah layout

---
+++ app/src/main/java/com/example/appkp/OnboardingTiga.kt
@@ -0,0 +1,11 @@
+package com.example.appkp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingTiga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_tiga)
+    }
+}
