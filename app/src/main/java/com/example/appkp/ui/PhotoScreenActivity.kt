95aa7cb59b6f7681bd4653f77128d8625448aba2
yofan2408 <yofan.ixe@gmail.com>
2020-06-26 10:20:46 +0700

    Base Photo Screen

---
+++ app/src/main/java/com/example/appkp/ui/PhotoScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appkp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appkp.R
+
+class PhotoScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_screen)
+    }
+}
