ed077e5de15fa5d78b7397854c3775846fb60f33
yofan2408 <yofan.ixe@gmail.com>
2020-06-26 16:35:55 +0700

    Membuat halaman unggah foto

--- app/src/main/java/com/example/appkp/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/appkp/ui/SplashScreenActivity.kt
@@ -6,20 +6,35 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.lifecycle.lifecycleScope
 import com.example.appkp.R
+import com.example.appkp.util.Preferences
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var preference: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
 
+        preference = Preferences(this)
+
+
         lifecycleScope.launch {
             delay(5000L)
-            startActivity(Intent(this@SplashScreenActivity, OnboardingActivity::class.java))
-            finish()
+
+
+            if (preference.getValue("isLoggedIn").equals("true")){
+                startActivity(Intent(this@SplashScreenActivity, PhotoScreenActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this@SplashScreenActivity, OnboardingActivity::class.java))
+                finish()
+            }
+
         }
     }
 }
