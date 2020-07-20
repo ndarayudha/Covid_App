35856faa4ee78f4c258fb67330661ef9fe31342c
langga799 <dikaaldika799@gmail.com>
2020-07-20 17:53:39 +0700

    Menambah Layout Riwayat dan Atikel

--- app/src/main/java/com/example/appkp/ui/dashboard/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/appkp/ui/dashboard/fragment/HomeFragment.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.example.appkp.R
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -22,17 +24,15 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         progressBar.apply {
-            progressMax = 100f
-            setProgressWithAnimation(50f, 1000)
-            progressBarWidth = 10f
-            backgroundProgressBarWidth = 15f
-            progressBarColor = Color.rgb(0, 255, 255)
-            backgroundProgressBarColor = Color.rgb(0, 0, 0)
+
+            setProgressWithAnimation(85f, 1000)
 
         }
     }
 }
+
 
 
 
